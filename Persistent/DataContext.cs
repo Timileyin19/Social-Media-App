026a0cc5dc57f11978bc3eaf4872d0b6c7f02367
Timileyin19 <abioduntimileyin19@gmail.com>
2021-01-23 13:08:25 +0100

    Initial Commit

---
+++ Persistent/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistent
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)        
+        { 
+        }
+
+        public DbSet<Value> Values { get; set; }  
+
+
+        protected override void OnModelCreating(ModelBuilder builder)      //  this is use to seed some data into the DB
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Ti Value"},
+                    new Value {Id = 2, Name = "Ta Value"},
+                    new Value {Id = 3, Name = "Tj Value"}
+                );
+        }   
+    }
+}
